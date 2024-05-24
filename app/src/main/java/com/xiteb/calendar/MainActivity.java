0bfdcce2bbd9b0f3ae9413303763c16dabae8161
Pubudu Sampath <sampathwit@gmail.com>
2024-05-24 17:45:21 +0530

    Initial commit

---
+++ app/src/main/java/com/xiteb/calendar/MainActivity.java
@@ -0,0 +1,19 @@
+package com.xiteb.calendar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CalendarView;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
