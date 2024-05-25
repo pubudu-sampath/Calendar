365ffd9891b07079f03047d49a65cd4e86202a55
Pubudu Sampath <sampathwit@gmail.com>
2024-05-26 01:27:13 +0530

    Added time adding function (Working)

--- app/src/main/java/com/xiteb/calendar/CreateEvent.java
+++ app/src/main/java/com/xiteb/calendar/CreateEvent.java
@@ -1,6 +1,7 @@
 package com.xiteb.calendar;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +9,7 @@ import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -24,15 +26,14 @@ import java.util.List;
 
 public class CreateEvent extends AppCompatActivity {
 
-     String titleInput;
-     String dateInput;
-//    private EditText startTimeInput;
-//    private EditText endTimeInput;
-     String noteInput;
-    String selectedDate;
-    ActivityCreateEventBinding binding;
-    FirebaseDatabase db;
-    DatabaseReference reference;
+     private String titleInput;
+     private String dateInput;
+     private String startTimeInput;
+     private String endTimeInput;
+     private String noteInput;
+    private ActivityCreateEventBinding binding;
+    private FirebaseDatabase db;
+    private DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,19 +44,35 @@ public class CreateEvent extends AppCompatActivity {
         binding.dateInput.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openDialog();
+                pickDate();
             }
         });
+
+        binding.startTimeInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pickTime(binding.startTimeInput);
+            }
+        });
+
+        binding.endTimeInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pickTime(binding.endTimeInput);
+            }
+        });
+
+
         binding.createEventButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 titleInput = binding.title.getText().toString();
                 noteInput = binding.note.getText().toString();
-
-
                 dateInput = binding.dateInput.getText().toString();
+                startTimeInput = binding.startTimeInput.getText().toString();
+                endTimeInput = binding.endTimeInput.getText().toString();
 
-                Event event = new Event(titleInput, noteInput, dateInput);
+                Event event = new Event(titleInput, noteInput, dateInput, startTimeInput, endTimeInput);
 
                 db = FirebaseDatabase.getInstance();
                 reference = db.getReference("Events");
@@ -67,19 +84,29 @@ public class CreateEvent extends AppCompatActivity {
                         binding.title.setText("");
                         binding.note.setText("");
                         binding.dateInput.setText("");
+                        binding.startTimeInput.setText("");
+                        binding.endTimeInput.setText("");
                         Toast.makeText(CreateEvent.this, "Added",Toast.LENGTH_SHORT).show();
 
                     }
                 });
-
-
             }
         });
-        
     }
 
-    private void openDialog() {
-        DatePickerDialog dialog =new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+    private void pickTime(EditText time) {
+        TimePickerDialog dialog = new TimePickerDialog(this, android.R.style.Theme_Holo_Light_DarkActionBar, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                String selectedTime = String.format("%02d:%02d", hourOfDay, minute);
+                time.setText(selectedTime);
+            }
+        },11,11, true);
+        dialog.show();
+    }
+
+    private void pickDate() {
+        DatePickerDialog dialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                 String selectedDate = String.format("%04d/%02d/%02d", year, month, dayOfMonth);
--- app/src/main/java/com/xiteb/calendar/Event.java
+++ app/src/main/java/com/xiteb/calendar/Event.java
@@ -2,22 +2,19 @@ package com.xiteb.calendar;
 
 public class Event {
 
-    String title;
-    String note;
-    String date;
+    private String title;
+    private String note;
+    private String date;
+    private String startTime;
+    private String endTime;
 
-    public Event(String title, String note, String date) {
+    public Event(String title, String note, String date, String startTime, String endTime) {
         this.title = title;
         this.note = note;
         this.date = date;
+        this.startTime = startTime;
+        this.endTime = endTime;
     }
-
-    public Event(String title, String note) {
-        this.title = title;
-        this.note = note;
-    }
-
-
 
     public String getTitle() {
         return title;
@@ -41,5 +38,21 @@ public class Event {
 
     public void setDate(String date) {
         this.date = date;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
     }
 }
